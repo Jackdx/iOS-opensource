804bfd72268ee511f0169728e53659aa46d8e510
Jackdx <dengxiang@dengxiangdeMacBook-Air.local>
2018-07-21 18:14:05 +0800

    初始化项目
    iOS-opensource of Foundation and UIKit framework

---
+++ iOS-opensource-Foundation/iOS-opensource-Foundation/iOS_opensource_Foundation.h
@@ -0,0 +1,19 @@
+//
+//  iOS_opensource_Foundation.h
+//  iOS-opensource-Foundation
+//
+//  Created by Jack on 2018/7/21.
+//  Copyright © 2018年 Jack. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for iOS_opensource_Foundation.
+FOUNDATION_EXPORT double iOS_opensource_FoundationVersionNumber;
+
+//! Project version string for iOS_opensource_Foundation.
+FOUNDATION_EXPORT const unsigned char iOS_opensource_FoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOS_opensource_Foundation/PublicHeader.h>
+
+
---
+++ iOS-opensource-UIKit/iOS-opensource-UIKit/iOS_opensource_UIKit.h
@@ -0,0 +1,19 @@
+//
+//  iOS_opensource_UIKit.h
+//  iOS-opensource-UIKit
+//
+//  Created by Jack on 2018/7/21.
+//  Copyright © 2018年 Jack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for iOS_opensource_UIKit.
+FOUNDATION_EXPORT double iOS_opensource_UIKitVersionNumber;
+
+//! Project version string for iOS_opensource_UIKit.
+FOUNDATION_EXPORT const unsigned char iOS_opensource_UIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOS_opensource_UIKit/PublicHeader.h>
+
+
